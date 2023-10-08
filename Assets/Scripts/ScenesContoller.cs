79c529eb4e06b3edff443fd55d36864b27a6da6c
Millan50 <1015aika256431@gmail.com>
2023-10-08 09:59:10 +0900

    ゲームオーバーを作った

---
+++ Assets/Scripts/ScenesContoller.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenesContoller : MonoBehaviour
+{
+    public MainController mainController;
+    public SheepController sheepController;
+    public SheepMeter sheepMeter;
+    float gameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sheepPercentage = sheepMeter.sheepPercentage;
+        if (sheepPercentage > 90 || sheepPercentage < 10)
+        {
+            gameOver += Time.deltaTime;
+            Debug.Log("count now");
+
+        }
+        else
+        {
+            gameOver = 0;
+        }
+        if (gameOver > 2.0f)
+        {
+            Debug.Log("GameOver");
+            gameOver = 0;
+        }
+    }
+}
